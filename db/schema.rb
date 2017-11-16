8bb1484873a2dbcc84f4aa88b3876f5079b98015
ryoheishioji <soltsoldier0730rs@gmail.com>
2017-11-16 16:58:44 +0900

    run 'rails db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116054344) do
+ActiveRecord::Schema.define(version: 20171116065411) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.string "body"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
     t.string "body"
     t.string "author"
     t.datetime "created_at", null: false
